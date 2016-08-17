363b2512d8ac6eab7a4fecce6dcd7ac5e98a6efc
BenediktKrueger <benekrue@de.bim.com>
2016-08-18 00:35:31 +0200

    Update lower-case.js

--- services/lower/lower-case.js
+++ services/lower/lower-case.js
@@ -5,6 +5,11 @@ module.exports = function(RED) {
     api_key: 'b242de56e40b4d1393f99f77b3e231d7f2314a98'    
     });
     
+    
+    var parameters = {
+     url: 'http://www-03.ibm.com/press/us/en/pressrelease/49384.wss'
+    };
+    
     function LowerCaseNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
