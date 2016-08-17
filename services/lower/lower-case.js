28afdf8d8cd7a38c52886f1cfec9fafd98f4cf93
BenediktKrueger <benekrue@de.bim.com>
2016-08-18 00:18:59 +0200

    Update lower-case.js

--- services/lower/lower-case.js
+++ services/lower/lower-case.js
@@ -1,31 +1,17 @@
 module.exports = function(RED) {
     
-    
-    
-    
-    
+    var watson = require('watson-developer-cloud');
+    var alchemy_language = watson.alchemy_language({
+    api_key: 'b242de56e40b4d1393f99f77b3e231d7f2314a98'    
+    });
     
     function LowerCaseNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
-        
-        var watson = require('watson-developer-cloud');
-        var alchemy_language = watson.alchemy_language({
-        api_key: 'b242de56e40b4d1393f99f77b3e231d7f2314a98'
-        })
-
-        var parameters = {
-        url: 'http://www-03.ibm.com/press/us/en/pressrelease/49384.wss'
-        };
-
-        alchemy_language.entities(parameters, function (err, response) {
-        if (err)
-        
-        else
-        msg.payload = JSON.stringify(response, null, 2);
-        node.send(msg);
+        this.on('input', function(msg) {
+            msg.payload = msg.payload.toLowerCase();
+            node.send(msg);
         });
-    
     }
     RED.nodes.registerType("lower-case",LowerCaseNode);
 }
