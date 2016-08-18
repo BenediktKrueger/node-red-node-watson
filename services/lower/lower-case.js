8a5858d902488b970815c76dade613a1fce2d0a1
BenediktKrueger <benekrue@de.bim.com>
2016-08-18 12:59:58 +0200

    Update lower-case.js

--- services/lower/lower-case.js
+++ services/lower/lower-case.js
@@ -7,14 +7,15 @@ module.exports = function(RED) {
     
     var watson = require('watson-developer-cloud');
     var alchemy_language = watson.alchemy_language({
-    api_key: msg.key1 || 'b242de56e40b4d1393f99f77b3e231d7f2314a98'    
+    api_key: 'b242de56e40b4d1393f99f77b3e231d7f2314a98'    
     });
+    
     var def = "";
     var fail = "";
     
     var parameters = {
     /*url: 'http://www-03.ibm.com/press/us/en/pressrelease/49384.wss'*/
-    text: 'Now hes impersonating a presidential candidate. That, too, used to be fun. He played a wretched character who humiliated anyone who stood in his way: immigrants, women, Muslims, the disabled, veterans and his Republican rivals, who keeled over one by one -- "Little Marco," "Low-Energy Jeb," "Lyin Ted."'
+    text: msg.payload 
     };
     
     alchemy_language.entities(parameters, function (err, response) {
