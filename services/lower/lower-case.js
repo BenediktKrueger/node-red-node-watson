71f384b99f0a66e319c7442020840e8b012ddc24
BenediktKrueger <benekrue@de.bim.com>
2016-08-18 11:42:39 +0200

    Update lower-case.js

--- services/lower/lower-case.js
+++ services/lower/lower-case.js
@@ -1,26 +1,24 @@
 module.exports = function(RED) {
     
     var watson = require('watson-developer-cloud');
-
+    
     var def = "";
     var fail = "";
     
     var parameters = {
     /*url: 'http://www-03.ibm.com/press/us/en/pressrelease/49384.wss'*/
-    text: 'test test test test'
+    text: 'Now hes impersonating a presidential candidate. That, too, used to be fun. He played a wretched character who humiliated anyone who stood in his way: immigrants, women, Muslims, the disabled, veterans and his Republican rivals, who keeled over one by one -- "Little Marco," "Low-Energy Jeb," "Lyin Ted."'
     };
     
     function LowerCaseNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
-        this.on('input', function(msg) {
-            
-        });
-    
+        
     this.doCall = function(msg) {
-    var alchemy_language = watson.alchemy_language({
-    api_key: 'b242de56e40b4d1393f99f77b3e231d7f2314a98'    
-    });
+      var alchemy_language = watson.alchemy_language({
+        api_key: msg.key    
+        });
+    }
     
     alchemy_language.entities(parameters, function (err, response) {
     if (err)
@@ -29,11 +27,11 @@ module.exports = function(RED) {
     def = JSON.stringify(response, null, 2);
     });
     
-    msg.payload = def + "---" + fail;
-    node.send(msg);
+    this.on('input', function(msg) {
+            msg.payload = def + "---" + fail;
+            node.send(msg);
+        });
     
-    };
-    };
-    
+    }
     RED.nodes.registerType("lower-case",LowerCaseNode);
-};
+}
