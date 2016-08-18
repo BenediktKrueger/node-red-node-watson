2fed2192dadbb7ee015d822e2f70a598d1f3e497
BenediktKrueger <benekrue@de.bim.com>
2016-08-18 13:54:13 +0200

    Update lower-case.js

--- services/lower/lower-case.js
+++ services/lower/lower-case.js
@@ -4,33 +4,34 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
     
-    node.apikey = 'b242de56e40b4d1393f99f77b3e231d7f2314a98';
-    node.test = msg.payload;
-    
-    var watson = require('watson-developer-cloud');
-    var alchemy_language = watson.alchemy_language({
-    api_key: node.apikey     
-    });
-    
-    var def = "";
-    var fail = "";
-    
-    var parameters = {
-    /*url: 'http://www-03.ibm.com/press/us/en/pressrelease/49384.wss'*/
-    text: 'Now hes impersonating a presidential candidate. That, too, used to be fun. He played a wretched character who humiliated anyone who stood in his way: immigrants, women, Muslims, the disabled, veterans and his Republican rivals, who keeled over one by one -- "Little Marco," "Low-Energy Jeb," "Lyin Ted."' 
-    };
-    
-    alchemy_language.entities(parameters, function (err, response) {
-    if (err)
-    fail = err;
-    else
-    def = JSON.stringify(response, null, 2);
-    });
     
         this.on('input', function(msg) {
-            msg.payload = def + "---" + fail;
-            msg.test = node.test;
-            node.send(msg);
+            
+        node.apikey = 'b242de56e40b4d1393f99f77b3e231d7f2314a98';
+        var watson = require('watson-developer-cloud');
+        var alchemy_language = watson.alchemy_language({
+        api_key: node.apikey     
+        });
+    
+        var def = "";
+        var fail = "";
+    
+        var parameters = {
+        /*url: 'http://www-03.ibm.com/press/us/en/pressrelease/49384.wss'*/
+        text: 'Now hes impersonating a presidential candidate. That, too, used to be fun. He played a wretched character who humiliated anyone who stood in his way: immigrants, women, Muslims, the disabled, veterans and his Republican rivals, who keeled over one by one -- "Little Marco," "Low-Energy Jeb," "Lyin Ted."' 
+        };
+    
+        alchemy_language.entities(parameters, function (err, response) {
+        if (err)
+        fail = err;
+        else
+        def = JSON.stringify(response, null, 2);
+        });
+            
+            
+        msg.payload = def + "---" + fail;
+        msg.test = node.test;
+        node.send(msg);
         });
     }
     RED.nodes.registerType("lower-case",LowerCaseNode);
