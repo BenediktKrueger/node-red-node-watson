bfabcc9f0cef17e0b0be30b57f2ed10fe0a61183
BenediktKrueger <benekrue@de.bim.com>
2016-08-18 12:00:29 +0200

    Update lower-case.js

--- services/lower/lower-case.js
+++ services/lower/lower-case.js
@@ -7,7 +7,7 @@ module.exports = function(RED) {
     
     var watson = require('watson-developer-cloud');
     var alchemy_language = watson.alchemy_language({
-    api_key: msg.key    
+    api_key: 'b242de56e40b4d1393f99f77b3e231d7f2314a98'    
     });
     var def = "";
     var fail = "";
