85f856c400341c69007ae8931a5787f57960df96
BenediktKrueger <benekrue@de.bim.com>
2016-08-19 10:51:00 +0200

    Update lower-case.js

--- services/lower/lower-case.js
+++ services/lower/lower-case.js
@@ -4,20 +4,20 @@ module.exports = function(RED) {
     
     function LowerCaseNode(config) {
     RED.nodes.createNode(this,config);
-    var globalContext = this.context().global;
+    //var globalContext = this.context().global;
     
     this.on('input', function(msg) {
     var key = msg.key;
     var input = msg.payload; 
     
-    globalContext.set("key", msg.key);
-    globalContext.set("input", msg.payload);
+    //globalContext.set("key", msg.key);
+    //globalContext.set("input", msg.payload);
     
 
     
     });
     
-    msg = {payload: globalContext.get("key")},          
+    msg = {payload: key},          
     this.send(msg);
     
     
